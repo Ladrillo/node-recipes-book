f4d95d10953e8e0624c7d122a5062a9b4a09989d
Ladrillo <gabriel@cabrejas.net>
2021-11-27 13:15:27 +0100

    edit canvas csv

--- 02-fs-modules.js
+++ 02-fs-modules.js
@@ -7,5 +7,35 @@ fs.writeFile(url, '.env\nnode_modules/\n.DS_Store\n.vscode')
     return fs.readFile(url)
   })
   .then(data => {
-    process.stdout.write(data)
+    // process.stdout.write(data)
   })
+
+function writeConvertedCsv() {
+  const nastyCsvUrl = new URL(
+    './assets/nasty.csv',
+    import.meta.url
+  )
+  const convertedCsvUrl = new URL(
+    './assets/nasty-converted.csv',
+    import.meta.url
+  )
+
+  fs.readFile(nastyCsvUrl)
+    .then(data => {
+      const lines = data
+        .toString()
+        .trim()
+        .split('\n')
+        .slice(2)
+        .filter(line => line.slice(0, 8) !== '"Student')
+      process.stdout.write(lines[lines.length - 1])
+      return fs.writeFile(convertedCsvUrl, lines.join('\n'))
+    })
+    .then(() => {
+      return fs.readFile(convertedCsvUrl)
+    })
+    .then((data) => {
+      process.stdout.write(data.toString())
+    })
+}
+writeConvertedCsv()
